6e6c848af1512d84682104d1a494aa623b0fee35
jungsikJeong <wndtlr1024@gmail.com>
2025-06-29 18:28:24 +0900

    사용자 서비스의 인증 모듈에서 auth.ts 파일을 삭제하고, .env.example 파일에 환경 변수 생성 안내를 추가함. app.module.ts에서 환경 파일 경로를 설정하고 JwtAuthGuard를 전역 가드로 등록함. role.enum.ts에 TODO 주석을 추가하여 역할 스코프 확인 필요성을 명시함.

--- apps/user-service/src/app.module.ts
+++ apps/user-service/src/app.module.ts
@@ -4,11 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { userSchema } from '../database/drizzle/schema';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
     DbModule.forRoot({
       config: {
@@ -18,8 +21,15 @@ import { UsersModule } from './users/users.module';
       },
       schema: userSchema,
     }),
+
     AuthModule,
     UsersModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
 export class AppModule {}
--- apps/user-service/src/auth/enums/role.enum.ts
+++ apps/user-service/src/auth/enums/role.enum.ts
@@ -1,3 +1,4 @@
+// TODO: 체인지 혹은 Role 정확히 어떤 스코프,,,등등있는지 체크?
 export enum Role {
   User = 'user',
   Admin = 'admin',
