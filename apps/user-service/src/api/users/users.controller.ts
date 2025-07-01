d2e1189c570fa28724314f6e309dd955f8910227
JungSik Jeong <wndtlr1024@gmail.com>
2025-07-01 14:06:52 +0900

    feat: users/me 구현

--- apps/user-service/src/api/users/users.controller.ts
+++ apps/user-service/src/api/users/users.controller.ts
@@ -6,12 +6,22 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from '../../commons/decorators/current-user.decorator';
+import { User } from 'apps/user-service/database/drizzle/schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  // TODO: 관리자를 위한  유저 조회
+
+  @Get('me')
+  @UseGuards(AuthGuard('jwt'))
+  async getMe(@CurrentUser() user: User) {
+    // todo: 비즈프로필 join까지 같이
+    return user;
+  }
 }
--- apps/user-service/src/commons/decorators/current-user.decorator.ts
+++ apps/user-service/src/commons/decorators/current-user.decorator.ts
@@ -1,12 +1,14 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserSchema } from 'apps/user-service/database/drizzle/schema';
+import { User } from 'apps/user-service/database/drizzle/schema';
 import { FastifyRequest } from 'fastify';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const request = context
       .switchToHttp()
-      .getRequest<FastifyRequest & { user: UserSchema }>();
-    return request.user;
+      .getRequest<FastifyRequest & { user: User }>();
+
+    const { password, ...userWithoutPassword } = request.user;
+    return userWithoutPassword;
   },
 );
