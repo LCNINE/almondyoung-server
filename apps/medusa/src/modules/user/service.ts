adfabf1df45a08b753f71c8a56fcce0c954327d0
jungsikJeong <wndtlr1024@gmail.com>
2025-07-15 13:28:45 +0900

    refactor: 로그 삭제

--- apps/medusa/src/modules/user/service.ts
+++ apps/medusa/src/modules/user/service.ts
@@ -36,7 +36,7 @@ export default class UserModuleService {
   async retrieveUser(userId: string): Promise<User> {
     try {
       const response = await this.client.get(`/users/${userId}`);
-      console.log('response:', response.data.data);
+
       return response.data.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
