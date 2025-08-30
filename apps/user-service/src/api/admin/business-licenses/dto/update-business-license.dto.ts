64ba7e0ca25ce867ce4933498ebb46658e1cb18e
jungsikJeong <wndtlr1024@gmail.com>
2025-08-30 15:25:02 +0900

    refactor: 어드민에있는 비지니스 로직 삭제

--- apps/user-service/src/api/admin/business-licenses/dto/update-business-license.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBusinessLicenseDto } from './create-business-license.dto';
-
-export class UpdateBusinessLicenseDto extends PartialType(
-  CreateBusinessLicenseDto,
-) {}
