d117d247482d4a168108e0f0231b75fc58800f15
jungsikJeong <wndtlr1024@gmail.com>
2025-09-03 17:13:11 +0900

    refactor: 결제 라우트 주석처리

--- apps/medusa/src/api/store/carts/[id]/complete/route.ts
+++ apps/medusa/src/api/store/carts/[id]/complete/route.ts
@@ -1,74 +1,76 @@
-import {
-  MedusaRequest,
-  MedusaResponse,
-  prepareRetrieveQuery,
-} from '@medusajs/framework/http';
-import { ContainerRegistrationKeys } from '@medusajs/framework/utils';
-import { completeCartWorkflow } from '@medusajs/medusa/core-flows';
-import { MedusaError } from '@medusajs/utils';
-import { refetchCart } from '../../helpers';
-import { defaultStoreCartFields } from '../../query-config';
+// 일부러 주석처리했음
 
-export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
-  const cart_id = req.params.id;
+// import {
+//   MedusaRequest,
+//   MedusaResponse,
+//   prepareRetrieveQuery,
+// } from '@medusajs/framework/http';
+// import { ContainerRegistrationKeys } from '@medusajs/framework/utils';
+// import { completeCartWorkflow } from '@medusajs/medusa/core-flows';
+// import { MedusaError } from '@medusajs/utils';
+// import { refetchCart } from '../../helpers';
+// import { defaultStoreCartFields } from '../../query-config';
 
-  // TODO: 이 워크플로우를 커스텀해야함
-  const { errors, result } = await completeCartWorkflow(req.scope).run({
-    input: { id: cart_id },
-    context: { transactionId: cart_id },
-    throwOnError: false,
-  });
+// export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
+//   const cart_id = req.params.id;
 
-  const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
+//   // TODO: 이 워크플로우를 커스텀해야함
+//   const { errors, result } = await completeCartWorkflow(req.scope).run({
+//     input: { id: cart_id },
+//     context: { transactionId: cart_id },
+//     throwOnError: false,
+//   });
 
-  // 워크플로우에서 에러가 발생하면, 이는 주로 장바구니 유효성 검사, 결제,
-  // 또는 재고 확인과 관련이 있습니다. 여기서는 소비자가 추가 조치를 취하고
-  // 문제를 해결할 수 있도록 에러와 함께 장바구니를 반환합니다.
-  if (errors?.[0]) {
-    const error = errors[0].error;
-    const statusOKErrors: string[] = [
-      // TODO: 재고 관련 특정 에러들 추가 필요
-      MedusaError.Types.PAYMENT_AUTHORIZATION_ERROR,
-      MedusaError.Types.PAYMENT_REQUIRES_MORE_ERROR,
-    ];
+//   const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
 
-    // statusOKErrors 목록에 없는 에러가 발생하면, 이는 장바구니가 완료될 수 있는
-    // 상태가 아니라는 의미입니다. 이런 경우에는 400 에러를 반환합니다.
-    const cart = await refetchCart(
-      cart_id,
-      req.scope,
-      prepareRetrieveQuery(
-        {},
-        {
-          defaults: defaultStoreCartFields,
-        },
-      ).remoteQueryConfig.fields,
-    );
+//   // 워크플로우에서 에러가 발생하면, 이는 주로 장바구니 유효성 검사, 결제,
+//   // 또는 재고 확인과 관련이 있습니다. 여기서는 소비자가 추가 조치를 취하고
+//   // 문제를 해결할 수 있도록 에러와 함께 장바구니를 반환합니다.
+//   if (errors?.[0]) {
+//     const error = errors[0].error;
+//     const statusOKErrors: string[] = [
+//       // TODO: 재고 관련 특정 에러들 추가 필요
+//       MedusaError.Types.PAYMENT_AUTHORIZATION_ERROR,
+//       MedusaError.Types.PAYMENT_REQUIRES_MORE_ERROR,
+//     ];
 
-    if (!statusOKErrors.includes(error.type)) {
-      throw error;
-    }
+//     // statusOKErrors 목록에 없는 에러가 발생하면, 이는 장바구니가 완료될 수 있는
+//     // 상태가 아니라는 의미입니다. 이런 경우에는 400 에러를 반환합니다.
+//     const cart = await refetchCart(
+//       cart_id,
+//       req.scope,
+//       prepareRetrieveQuery(
+//         {},
+//         {
+//           defaults: defaultStoreCartFields,
+//         },
+//       ).remoteQueryConfig.fields,
+//     );
 
-    res.status(200).json({
-      type: 'cart',
-      cart,
-      error: {
-        message: error.message,
-        name: error.name,
-        type: error.type,
-      },
-    });
-    return;
-  }
+//     if (!statusOKErrors.includes(error.type)) {
+//       throw error;
+//     }
 
-  const { data } = await query.graph({
-    entity: 'order',
-    fields: req.queryConfig.fields,
-    filters: { id: result.id },
-  });
+//     res.status(200).json({
+//       type: 'cart',
+//       cart,
+//       error: {
+//         message: error.message,
+//         name: error.name,
+//         type: error.type,
+//       },
+//     });
+//     return;
+//   }
 
-  res.status(200).json({
-    type: 'order',
-    order: data[0],
-  });
-};
+//   const { data } = await query.graph({
+//     entity: 'order',
+//     fields: req.queryConfig.fields,
+//     filters: { id: result.id },
+//   });
+
+//   res.status(200).json({
+//     type: 'order',
+//     order: data[0],
+//   });
+// };
