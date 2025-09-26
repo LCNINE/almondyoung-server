5736c385c21af7dd0c837607c28796bef40028df
jungsikJeong <wndtlr1024@gmail.com>
2025-09-26 13:00:53 +0900

    refactor: shop-info 변경

--- apps/user-service/src/api/shop/shop.controller.ts
+++ apps/user-service/src/api/shop/shop.controller.ts
@@ -1,26 +1,16 @@
+import { AuthorizationGuard, RequireScopes } from '@app/roles';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
-import {
-  ApiTags,
+  ApiBearerAuth,
   ApiOperation,
   ApiResponse,
-  ApiBearerAuth,
+  ApiTags,
 } from '@nestjs/swagger';
-import { ShopService } from './shop.service';
-import { CreateShopInfoDto } from './dto/create-shop-info.dto';
-import { UpdateShopInfoDto } from './dto/update-shop-info';
-import { JwtAuthGuard } from '../../commons/guards/jwt-auth.guard';
-import { CurrentUser } from '../../commons/decorators/current-user.decorator';
 import { User } from 'apps/user-service/database/drizzle/schema';
-import { AuthorizationGuard, RequireScopes } from '@app/roles';
+import { CurrentUser } from '../../commons/decorators/current-user.decorator';
+import { JwtAuthGuard } from '../../commons/guards/jwt-auth.guard';
+import { CreateShopInfoDto } from './dto/create-shop-info.dto';
+import { ShopService } from './shop.service';
 
 @ApiTags('Shop')
 @ApiBearerAuth('access-token')
@@ -37,24 +27,11 @@ export class ShopController {
     return this.shopService.findOneByUserId(user.id);
   }
 
-  @ApiOperation({ summary: '상점 정보 생성' })
+  @ApiOperation({ summary: '상점 정보 생성 및 수정' })
   @ApiResponse({ status: 201, description: '상점 정보 생성 성공' })
   @Post('info')
   @RequireScopes(['user:modify'])
-  create(@Body() createShopDto: CreateShopInfoDto, @CurrentUser() user: User) {
-    console.log('createShopDto:', createShopDto);
-    return this.shopService.create(createShopDto, user);
-  }
-
-  @ApiOperation({ summary: '상점 정보 수정' })
-  @ApiResponse({ status: 200, description: '상점 정보 수정 성공' })
-  @Patch(':id/info')
-  @RequireScopes(['user:modify'])
-  update(
-    @Param('id') id: string,
-    @Body() updateShopDto: UpdateShopInfoDto,
-    @CurrentUser() user: User,
-  ) {
-    return this.shopService.update(id, updateShopDto, user);
+  modify(@Body() createShopDto: CreateShopInfoDto, @CurrentUser() user: User) {
+    return this.shopService.modify(createShopDto, user);
   }
 }
