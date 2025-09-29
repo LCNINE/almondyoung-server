d471cf9ac2f21ba034c38b55b9e2063aea32b99c
PauseB <116489192+PauseB@users.noreply.github.com>
2025-09-30 00:18:04 +0900

    drizzle 세팅 개선

--- apps/user-service/database/drizzle/drizzle.config.ts
+++ apps/user-service/database/drizzle/drizzle.config.ts
@@ -2,14 +2,14 @@ import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 import { join } from 'path';
 
-config({ path: join(__dirname, '../../.env') });
+config({ path: join(__dirname, '../../.env'), override: true });
 
 export default defineConfig({
   out: './apps/user-service/database/drizzle',
   schema: './apps/user-service/database/drizzle/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://almond-users-service_owner:npg_PESMZpX6nu5L@ep-jolly-river-a8oplnnc-pooler.eastus2.azure.neon.tech/almond-users-service?sslmode=require',
+    url: process.env.DATABASE_URL || '',
   },
   verbose: true,
 });
