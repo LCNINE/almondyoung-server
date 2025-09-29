312ef5d50861397881d05d75b278e124d8e00c65
jihun <gn753@naver.com>
2025-09-29 15:42:03 +0900

    uuid v7 삽입안되던에러수정

--- apps/pim/src/main.ts
+++ apps/pim/src/main.ts
@@ -6,12 +6,22 @@ import { PimModule } from './pim.module';
 async function bootstrap() {
   const app = await NestFactory.create(PimModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     transform: true,
+  //     forbidNonWhitelisted: true,
+  //     disableErrorMessages: false,
+  //     validationError: { target: false, value: false },
+  //   }),
+  // );
   app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('PIM API')
-    .setDescription('상품 정보 관리 시스템 (Product Information Management) API')
+    .setDescription(
+      '상품 정보 관리 시스템 (Product Information Management) API',
+    )
     .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
