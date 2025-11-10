79279e432fd53c4347b616bccce7f7e74f514e1c
jungsikJeong <wndtlr1024@gmail.com>
2025-11-10 10:27:29 +0900

    refactor: Public 추가

--- apps/user-service/src/api/user-main.controller.ts
+++ apps/user-service/src/api/user-main.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../constants/public.decorator';
 import { UserMainService } from './user-main.service';
 
 @Controller()
@@ -6,6 +7,7 @@ export class UserMainController {
   constructor(private readonly userMainService: UserMainService) {}
 
   @Get()
+  @Public()
   getHello(): string {
     return this.userMainService.getHello();
   }
