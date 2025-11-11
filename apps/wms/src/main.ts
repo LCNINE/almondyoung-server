8b84c1b5a914ca7338b978cdba8ea3ab4fad2c71
PauseB <116489192+PauseB@users.noreply.github.com>
2025-11-11 13:03:14 +0900

    phase 3

--- apps/wms/src/main.ts
+++ apps/wms/src/main.ts
@@ -3,12 +3,22 @@ import { NestFactory } from '@nestjs/core';
 import { WmsModule } from './wms.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { EventsModule } from '@app/events';
+import { PRODUCT_STREAM } from '@packages/event-contracts';
 
 async function bootstrap() {
   const app = await NestFactory.create(WmsModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
+  app.enableShutdownHooks();
+
+  const consumerOptions = EventsModule.forConsumer({
+    streams: [PRODUCT_STREAM],
+    groupId: 'wms-product-consumer',
+  });
+
+  app.connectMicroservice(consumerOptions);
 
   const config = new DocumentBuilder()
     .setTitle('WMS API')
@@ -18,6 +28,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3010);
 }
 bootstrap();
