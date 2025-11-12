f5a61e51f169a5e260832744e65cbf69ed24c67b
jungsikJeong <wndtlr1024@gmail.com>
2025-11-12 13:00:10 +0900

    refactor: jwt-auth.guard 콜백함수 삭제

--- apps/user-service/src/commons/guards/jwt-auth.guard.ts
+++ apps/user-service/src/commons/guards/jwt-auth.guard.ts
@@ -1,22 +1,14 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../../constants/public.decorator';
-
-interface UserPayload {
-  userId: string;
-  nickname: string;
-}
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
     super();
   }
+
   canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
@@ -27,12 +19,5 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       return true;
     }
     return super.canActivate(context);
-  }
-
-  handleRequest<TUser = UserPayload>(err: any, user: TUser): TUser {
-    if (err || !user) {
-      throw err || new UnauthorizedException('사용자를 찾을 수 없습니다.');
-    }
-    return user;
   }
 }
