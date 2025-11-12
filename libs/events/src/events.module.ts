9c739266960aa8caaa00cbde2f7acddfe0a9d69a
PauseB <116489192+PauseB@users.noreply.github.com>
2025-11-12 15:26:47 +0900

    Squashed commit of the following:
    commit 36a23a5469f42a722ad16fe17296551f3bb5c6ad
    Author: PauseB <116489192+PauseB@users.noreply.github.com>
    Date:   Wed Nov 12 06:34:00 2025 +0900
    
        removed logging
    
    commit 1441156dd60a24ebd89b12e29a8affc98046ca5f
    Author: PauseB <116489192+PauseB@users.noreply.github.com>
    Date:   Wed Nov 12 04:54:06 2025 +0900
    
        additional logging
    
    commit ff3d9f46cda66b1432dc74549f8c86ad0085c229
    Author: PauseB <116489192+PauseB@users.noreply.github.com>
    Date:   Wed Nov 12 04:49:31 2025 +0900
    
        logging
    
    commit 5fd1c1f88f4a2252268cc4652834b1905dff0ea4
    Author: PauseB <116489192+PauseB@users.noreply.github.com>
    Date:   Wed Nov 12 04:35:51 2025 +0900
    
        import 방식 오타 수정
    
    commit def318bd82681a7f33b7b399afbafc611d35ba76
    Author: PauseB <116489192+PauseB@users.noreply.github.com>
    Date:   Wed Nov 12 03:59:13 2025 +0900
    
        WMS에 다른 앱과 같은 kafka 세팅
    
    commit 43cba074f45abd35c96c67ae3ed08b837a339681
    Author: PauseB <116489192+PauseB@users.noreply.github.com>
    Date:   Wed Nov 12 03:01:10 2025 +0900
    
        APP_INTERCEPTOR가 export되지 않도록 수정

--- libs/events/src/events.module.ts
+++ libs/events/src/events.module.ts
@@ -242,13 +242,8 @@ export class EventsModule {
       ],
       providers,
       exports: providers
-        .filter((p) => p.provide !== APP_FILTER)
+        .filter((p) => p.provide !== APP_FILTER && p.provide !== APP_INTERCEPTOR)
         .map((p) => p.provide),
-      // exports: providers
-      //   .filter(
-      //     (p) => p.provide !== APP_FILTER && p.provide !== APP_INTERCEPTOR,
-      //   )
-      //   .map((p) => p.provide),
     };
   }
 
