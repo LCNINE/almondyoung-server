533a3a493f8431332f8b09377cb0596045cc2d42
jungsikJeong <wndtlr1024@gmail.com>
2025-11-12 11:09:07 +0900

    refactor:  1. app.module에서 config 추가 2. 네온디비 dev, pro 쓰는 관계로 의미가없어진 seed 추가하는거 삭제 (이걸로인해서 좀 더 빠른 로딩이됌) 3. 어플리케이션 시작 멘트 변경

--- apps/user-service/src/api/consents/consents.module.ts
+++ apps/user-service/src/api/consents/consents.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'apps/user-service/database/database.module';
 import { ConsentsController } from './consents.controller';
 import { ConsentsService } from './consents.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [],
   controllers: [ConsentsController],
   providers: [ConsentsService],
   exports: [ConsentsService],
