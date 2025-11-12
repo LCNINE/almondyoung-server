ddff3a1cc92bcefc0b4603b37ce90e262bcdd264
jungsikJeong <wndtlr1024@gmail.com>
2025-11-12 23:52:47 +0900

    refactor: 번호 조회 dto 수정

--- apps/user-service/src/api/twilio/dto/twilio.dto.ts
+++ apps/user-service/src/api/twilio/dto/twilio.dto.ts
@@ -1,9 +1,12 @@
 import { PickType } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class SendMessageDto {
   @IsString({ message: '전화번호는 문자열이어야 합니다.' })
   @IsNotEmpty({ message: '전화번호는 필수 입력 항목입니다.' })
+  @Matches(/^\+[1-9]\d{7,14}$/, {
+    message: '전화번호는 E.164 국제 표준 형식이어야 합니다.',
+  })
   phoneNumber: string;
 
   @IsString({ message: '메시지는 문자열이어야 합니다.' })
