1eacd313e5ee9a9d1a7cbb71347cef653fe82f27
jungsikJeong <wndtlr1024@gmail.com>
2025-11-13 14:07:25 +0900

    feat: 만료된 인증번호 크론 기능 추가

--- apps/user-service/src/api/twilio/services/expire-existing-codes.ts
+++ apps/user-service/src/api/twilio/services/expire-existing-codes.ts
@@ -1,12 +1,16 @@
 import { DbService, InjectDb } from '@app/db';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import {
   userServiceSchema,
   UserServiceSchema,
 } from 'apps/user-service/database/drizzle/schema';
 import { DbTransaction } from 'apps/user-service/src/commons/types';
-import { eq } from 'drizzle-orm';
+import { eq, lt } from 'drizzle-orm';
 
+@Injectable()
 export class ExpireExistingCodesService {
+  private readonly logger = new Logger(ExpireExistingCodesService.name);
   constructor(
     @InjectDb() private readonly dbService: DbService<UserServiceSchema>,
   ) {}
@@ -24,5 +28,23 @@ export class ExpireExistingCodesService {
         isExpired: true,
       })
       .where(eq(userServiceSchema.phoneVerifications.phoneNumber, phoneNumber));
+  }
+
+  @Cron('0 0 * * *') // 매일 자정 실행
+  async cleanupExpiredVerifications(tx?: DbTransaction) {
+    this.logger.log('만료된 핸드폰 번호 인증 코드 삭제 작업 시작');
+    const client = this.getClient(tx);
+
+    // 24시간 전 시간
+    const twentyFourHoursAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
+
+    // 24시간 이전에 만료된 레코드들만 삭제
+    await client
+      .delete(userServiceSchema.phoneVerifications)
+      .where(
+        lt(userServiceSchema.phoneVerifications.expiresAt, twentyFourHoursAgo),
+      );
+
+    this.logger.log('만료된 핸드폰 번호 인증 코드 삭제 작업 완료');
   }
 }
