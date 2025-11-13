4ae96552f4f1062b53e2f4651513975574c742d0
jungsikJeong <wndtlr1024@gmail.com>
2025-11-13 18:02:00 +0900

    refactor(business): 사업자 조회 외부 api로 (bizno사용) 대표자이름 일치하는지 확인하는 기능 리펙토링

--- apps/user-service/src/api/business-licenses/business-licenses.module.ts
+++ apps/user-service/src/api/business-licenses/business-licenses.module.ts
@@ -1,10 +1,13 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { BusinessLicensesController } from './business-licenses.controller';
+import { BusinessLicensesHelper } from './business-licenses.helper';
 import { BusinessLicensesService } from './business-licenses.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [BusinessLicensesController],
-  providers: [BusinessLicensesService],
+  providers: [BusinessLicensesService, BusinessLicensesHelper],
   exports: [BusinessLicensesService],
 })
 export class BusinessLicensesModule {}
