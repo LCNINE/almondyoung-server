ec0f7439f5e879362505104e5c9fd1a6bd2c3b8e
PauseB <116489192+PauseB@users.noreply.github.com>
2025-11-21 18:10:39 +0900

    초기 구현

--- apps/pim/src/core/channels/channels.module.ts
+++ apps/pim/src/core/channels/channels.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
 import { SalesChannelsController } from './sales-channels.controller';
 import { ChannelProductsController } from './channel-products.controller';
+import { ChannelCategoriesController } from './channel-categories.controller';
 import { SalesChannelsService } from './sales-channels.service';
 import { ChannelProductsService } from './channel-products.service';
+import { ChannelCategoriesService } from './channel-categories.service';
 
 @Module({
-  controllers: [SalesChannelsController, ChannelProductsController],
-  providers: [SalesChannelsService, ChannelProductsService],
-  exports: [SalesChannelsService, ChannelProductsService],
+  controllers: [
+    SalesChannelsController,
+    ChannelProductsController,
+    ChannelCategoriesController,
+  ],
+  providers: [
+    SalesChannelsService,
+    ChannelProductsService,
+    ChannelCategoriesService,
+  ],
+  exports: [
+    SalesChannelsService,
+    ChannelProductsService,
+    ChannelCategoriesService,
+  ],
 })
 export class ChannelsModule {}
 
