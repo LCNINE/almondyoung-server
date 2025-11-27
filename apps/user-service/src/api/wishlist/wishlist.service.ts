d1baa3a79ff3f805c3e24569a00e9deb3691d6f4
jungsikJeong <wndtlr1024@gmail.com>
2025-11-27 11:46:43 +0900

    refactor: 로그삭제

--- apps/user-service/src/api/wishlist/wishlist.service.ts
+++ apps/user-service/src/api/wishlist/wishlist.service.ts
@@ -12,7 +12,7 @@ import { AddToWishlistDto } from './dto/wishlist.dto';
 
 @Injectable()
 export class WishlistService {
-  private readonly logger = new Logger(WishlistService.name);
+
 
   constructor(
     @InjectDb() private readonly dbService: DbService<UserServiceSchema>,
@@ -30,7 +30,7 @@ export class WishlistService {
       )
       .limit(1);
 
-    console.log('existing::::', existing);
+
 
     // 이미 찜목록에 있으면 제거
     if (existing.length > 0) {
@@ -43,12 +43,12 @@ export class WishlistService {
           ),
         );
 
-      console.log('deleted::');
+
 
       return
     }
 
-    console.log('====insert====');
+
 
     // 찜목록에 없으면 추가
     const result = await this.dbService.db
@@ -59,7 +59,7 @@ export class WishlistService {
       })
       .returning();
 
-    console.log('result::', result);
+
     return
   }
 
