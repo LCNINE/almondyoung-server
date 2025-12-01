afb037ac3dc955ba8bd89b8698f32b9b3b2a0356
jungsikJeong <wndtlr1024@gmail.com>
2025-12-01 18:43:20 +0900

    refactor: 유저서비스에서 UploadModule 참조하고있는거 제거

--- apps/user-service/src/api/business-licenses/business-licenses.module.ts
+++ apps/user-service/src/api/business-licenses/business-licenses.module.ts
@@ -1,12 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { UploadModule } from 'apps/file-service/src/upload/upload.module';
 import { BusinessLicensesController } from './business-licenses.controller';
 import { BusinessLicensesHelper } from './business-licenses.helper';
 import { BusinessLicensesService } from './business-licenses.service';
 
 @Module({
-  imports: [HttpModule, UploadModule],
+  imports: [HttpModule],
   controllers: [BusinessLicensesController],
   providers: [BusinessLicensesService, BusinessLicensesHelper],
   exports: [BusinessLicensesService],
