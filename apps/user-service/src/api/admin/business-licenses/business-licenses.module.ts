332d9add358abdfc94960d8b298003d46006860f
jungsikJeong <wndtlr1024@gmail.com>
2025-12-01 11:38:06 +0900

    fix: import UploadModule error

--- apps/user-service/src/api/admin/business-licenses/business-licenses.module.ts
+++ apps/user-service/src/api/admin/business-licenses/business-licenses.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UploadModule } from 'apps/file-service/src/upload/upload.module';
 import { BusinessLicensesController } from './business-licenses.controller';
 import { BusinessLicensesService } from './business-licenses.service';
 
 @Module({
+  imports: [UploadModule],
   controllers: [BusinessLicensesController],
   providers: [BusinessLicensesService],
   exports: [BusinessLicensesService],
 })
-export class AdminBusinessLicensesModule {}
+export class AdminBusinessLicensesModule { }
