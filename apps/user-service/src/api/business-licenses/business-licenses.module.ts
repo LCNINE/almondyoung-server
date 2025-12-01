3063289aaeac0d54a55638c86e6354876b5168f6
jungsikJeong <wndtlr1024@gmail.com>
2025-12-01 11:38:06 +0900

    fix: import UploadModule error

--- apps/user-service/src/api/business-licenses/business-licenses.module.ts
+++ apps/user-service/src/api/business-licenses/business-licenses.module.ts
@@ -1,13 +1,14 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { UploadModule } from 'apps/file-service/src/upload/upload.module';
 import { BusinessLicensesController } from './business-licenses.controller';
 import { BusinessLicensesHelper } from './business-licenses.helper';
 import { BusinessLicensesService } from './business-licenses.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, UploadModule],
   controllers: [BusinessLicensesController],
   providers: [BusinessLicensesService, BusinessLicensesHelper],
   exports: [BusinessLicensesService],
 })
-export class BusinessLicensesModule {}
+export class BusinessLicensesModule { }
