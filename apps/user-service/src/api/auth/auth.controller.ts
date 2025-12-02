16eaf230c5658d0ebe5dd1814789dd5353bad9dc
jungsikJeong <wndtlr1024@gmail.com>
2025-12-02 10:20:24 +0900

    test: console.log 소셜로그인 리다이렉트 유실되는 지점

--- apps/user-service/src/api/auth/auth.controller.ts
+++ apps/user-service/src/api/auth/auth.controller.ts
@@ -194,7 +194,7 @@ export class AuthController {
   @Get('kakao/signin')
   @UseGuards(AuthGuard('kakao'))
   @Public()
-  async kakaoAuth() {
+  async kakaoAuth(@Query('redirect_to') redirectTo?: string) {
     // 카카오 로그인 리다이렉트
   }
 
