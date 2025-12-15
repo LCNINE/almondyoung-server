b9312ca56d56be38b915fde97be53c7188682eef
jungsikJeong <wndtlr1024@gmail.com>
2025-12-15 16:27:50 +0900

    test: 토큰 시간 15초

--- apps/user-service/src/constants/auth.constant.ts
+++ apps/user-service/src/constants/auth.constant.ts
@@ -1,4 +1,4 @@
-const JWT_ACCESS_TOKEN_EXPIRATION = '30d'; // 액세스 토큰 만료 시간, todo: 15m로 운영할때는 고쳐야됌
+const JWT_ACCESS_TOKEN_EXPIRATION = '15s'; // 액세스 토큰 만료 시간, todo: 15m로 운영할때는 고쳐야됌
 const JWT_RESET_PASSWORD_ACCESS_TOKEN_EXPIRATION = '5m'; // 비밀번호 재설정 액세스 토큰 만료 시간
 const JWT_REFRESH_TOKEN_EXPIRATION = '2w'; // 일반 로그인 (2주)
 const JWT_REFRESH_TOKEN_LONG_EXPIRATION = '90d'; // 자동 로그인 (90일)
