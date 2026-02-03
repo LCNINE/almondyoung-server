034b4b4884bae9cca6a313a46b96223072d56d82
PauseB <116489192+PauseB@users.noreply.github.com>
2026-02-03 17:25:34 +0900

    [User] 카페24 계정 연결에 사용할 링크 토큰 교환 API 구현

--- apps/user-service/src/api/cafe24-link/cafe24-link.module.ts
+++ apps/user-service/src/api/cafe24-link/cafe24-link.module.ts
@@ -1,8 +1,10 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { Cafe24LinkController } from './cafe24-link.controller';
 import { Cafe24LinkService } from './cafe24-link.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [Cafe24LinkController],
   providers: [Cafe24LinkService],
   exports: [Cafe24LinkService],
