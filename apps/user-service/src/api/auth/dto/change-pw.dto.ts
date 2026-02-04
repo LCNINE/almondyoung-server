62880b23a3159c0bf59c0c0d29f0b57fe8741ecc
JungSik Jeong <wndtlr1024@gmail.com>
2026-02-04 11:43:29 +0900

    [User]: 비밀번호 확인, 비밀번호 변경 라우터를 하나로 합침 (#325)

--- apps/user-service/src/api/auth/dto/change-pw.dto.ts
+++ apps/user-service/src/api/auth/dto/change-pw.dto.ts
@@ -3,19 +3,36 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ChangePasswordDto {
   @ApiProperty({
+    description: '현재 비밀번호',
+    example: 'currentpassword123',
+    minLength: 8,
+    maxLength: 20,
+  })
+  @IsString({
+    message: '현재 비밀번호는 문자열이어야 합니다.',
+  })
+  @MinLength(8, {
+    message: '현재 비밀번호는 최소 8자 이상이어야 합니다.',
+  })
+  @MaxLength(20, {
+    message: '현재 비밀번호는 최대 20자 이하이어야 합니다.',
+  })
+  currentPassword: string;
+
+  @ApiProperty({
     description: '새 비밀번호',
     example: 'newpassword123',
     minLength: 8,
     maxLength: 20,
   })
   @IsString({
-    message: '비밀번호는 문자열이어야 합니다.',
+    message: '새 비밀번호는 문자열이어야 합니다.',
   })
   @MinLength(8, {
-    message: '비밀번호는 최소 8자 이상이어야 합니다.',
+    message: '새 비밀번호는 최소 8자 이상이어야 합니다.',
   })
   @MaxLength(20, {
-    message: '비밀번호는 최대 20자 이하이어야 합니다.',
+    message: '새 비밀번호는 최대 20자 이하이어야 합니다.',
   })
-  password: string;
+  newPassword: string;
 }
