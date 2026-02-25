01c170c3871841ada527d168e112adda05003fc3
PauseB <116489192+PauseB@users.noreply.github.com>
2026-02-26 05:56:54 +0900

    opensearch 권한 문제 해결

--- docker-entrypoint-opensearch.sh
+++ docker-entrypoint-opensearch.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Volume이 root 소유로 마운트될 수 있으므로 런타임에 권한 수정
 chown -R 1000:1000 /usr/share/opensearch/data 2>/dev/null || true
-
-exec su-exec opensearch /usr/share/opensearch/opensearch-docker-entrypoint.sh "$@"
+exec /usr/share/opensearch/opensearch-docker-entrypoint.sh "$@"
