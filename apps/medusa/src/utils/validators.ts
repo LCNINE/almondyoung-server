da980c7a088d0fad645c736d01558334537e8ac4
jungsikjeong <wndtlr1024@gmail.com>
2026-03-04 10:36:44 +0900

    [Medsusa] 미들웨어 me 요청에따른 오더 조회 에러로인해 삭제하고, 그냥 라우트에 groups를 포함하도록 오버라이딩

--- apps/medusa/src/utils/validators.ts
+++ apps/medusa/src/utils/validators.ts
@@ -5,3 +5,53 @@ export const createSelectParams = () => {
     fields: z.string().optional(),
   });
 };
+
+export const createFindParams = ({
+  offset,
+  limit,
+  order,
+}: {
+  offset?: number
+  limit?: number
+  order?: string
+} = {}) => {
+  const selectParams = createSelectParams()
+
+  return selectParams.merge(
+    z.object({
+      offset: z.preprocess(
+        (val) => {
+          if (val && typeof val === "string") {
+            return parseInt(val)
+          }
+          return val
+        },
+        z
+          .number()
+          .optional()
+          .default(offset ?? 0)
+      ),
+      limit: z.preprocess(
+        (val) => {
+          if (val && typeof val === "string") {
+            return parseInt(val)
+          }
+          return val
+        },
+        z
+          .number()
+          .optional()
+          .default(limit ?? 20)
+      ),
+      order: order
+        ? z.string().optional().default(order)
+        : z.string().optional(),
+      with_deleted: z.preprocess((val) => {
+        if (val && typeof val === "string") {
+          return val === "true" ? true : val === "false" ? false : val
+        }
+        return val
+      }, z.boolean().optional()),
+    })
+  )
+}
