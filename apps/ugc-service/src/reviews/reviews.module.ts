f8f9b636fd2b4a294adf2515e2e80454381d73d3
jungsikjeong <wndtlr1024@gmail.com>
2026-03-05 16:35:21 +0900

    [Ugc] : 리뷰 정책 api 추가 및 리뷰 카프카 발행 스트림추가

--- apps/ugc-service/src/reviews/reviews.module.ts
+++ apps/ugc-service/src/reviews/reviews.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ReviewsController } from './reviews.controller';
-import { ReviewsService } from './reviews.service';
+import { EventsModule } from '@app/events';
+import { UGC_COMMAND_STREAM } from '@packages/event-contracts/streams';
+import { ReviewsController } from './controllers/reviews.controller';
+import { RewardPolicyController } from './controllers/reward-policy.controller';
+import { ReviewsService } from './services/reviews.service';
+import { ReviewRewardPolicyService } from './services/review-reward-policy.service';
+import { ReviewRewardPublisher } from './services/review-reward-publisher.service';
 
 @Module({
-  controllers: [ReviewsController],
-  providers: [ReviewsService],
+  imports: [
+    EventsModule.forRoot({
+      streams: [UGC_COMMAND_STREAM],
+      serviceName: 'ugc-service',
+    }),
+  ],
+  controllers: [ReviewsController, RewardPolicyController],
+  providers: [ReviewsService, ReviewRewardPolicyService, ReviewRewardPublisher],
 })
 export class ReviewsModule {}
