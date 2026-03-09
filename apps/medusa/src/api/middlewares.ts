e1cd6de73d7ba06ada2dd16ebec7231149d5616f
jungsikjeong <wndtlr1024@gmail.com>
2026-03-09 13:13:54 +0900

    [Medusa] 유저 배송지 못불러오는 문제 미들웨어 주석처리

--- apps/medusa/src/api/middlewares.ts
+++ apps/medusa/src/api/middlewares.ts
@@ -4,15 +4,15 @@ import { adminRouteMiddlewares } from './admin/middlewares';
 export default defineMiddlewares({
   routes: [
     ...adminRouteMiddlewares,
-    {
-      matcher: '/store/customers/me',
-      middlewares: [
-        (req: any, _res: any, next: any) => {
-          (req.allowed ??= []).push('groups');
-          next();
-        },
-      ],
-    },
+    // {
+    //   matcher: '/store/customers/me',
+    //   middlewares: [
+    //     (req: any, _res: any, next: any) => {
+    //       (req.allowed ??= []).push('groups');
+    //       next();
+    //     },
+    //   ],
+    // },
     {
       matcher: '/store/customers/me/promotions',
       middlewares: [authenticate('customer', ['session', 'bearer'])],
