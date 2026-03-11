29400334224c2781a5a32713c1475974d0a30713
PauseB <116489192+PauseB@users.noreply.github.com>
2026-03-12 07:46:08 +0900

    [admin] 빌드를 막는 에러 수정

--- apps/admin-web/src/app/login/page.tsx
+++ apps/admin-web/src/app/login/page.tsx
@@ -1,5 +1,10 @@
 import LoginTemplate from '@/features/login/template/page';
+import { Suspense } from 'react';
 
-export default async function LoginPage() {
-  return <LoginTemplate />;
+export default function LoginPage() {
+  return (
+    <Suspense>
+      <LoginTemplate />
+    </Suspense>
+  );
 }
