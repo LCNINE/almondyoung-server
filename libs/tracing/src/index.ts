4220570b9da0018aec5217fb1d7e676bc68276e3
jungsikjeong <wndtlr1024@gmail.com>
2026-03-23 13:08:04 +0900

    feat: update OTel tracing to support Fastify

--- libs/tracing/src/index.ts
+++ libs/tracing/src/index.ts
@@ -1,6 +1,8 @@
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import { FastifyInstrumentation } from '@opentelemetry/instrumentation-fastify'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
 
 const endpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT
 const serviceName = process.env.OTEL_SERVICE_NAME
@@ -12,7 +14,10 @@ if (!endpoint || !serviceName) {
 } else {
   const sdk = new NodeSDK({
     traceExporter: new OTLPTraceExporter({ url: endpoint }),
-    instrumentations: [getNodeAutoInstrumentations()],
+    instrumentations: [
+      new HttpInstrumentation(),
+      new FastifyInstrumentation(),
+    ],
     serviceName,
   })
 
