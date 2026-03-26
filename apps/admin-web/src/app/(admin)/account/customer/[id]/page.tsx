7b532a2f010009a87003ef83e460a1f6fc6926a7
jungsikjeong <wndtlr1024@gmail.com>
2026-03-26 18:04:11 +0900

    [admin-web] : 메뉴에 '관리자 계정' 항목 추가

--- apps/admin-web/src/app/(admin)/account/customer/[id]/page.tsx
+++ apps/admin-web/src/app/(admin)/account/customer/[id]/page.tsx
@@ -1,8 +1,12 @@
-import { TwoColumnPage } from "@/components/admin-ui-experimental/layout";
-import { CustomerDetailGeneral } from "./customer-detail-general";
-import { CustomerDetailShop } from "./customer-detail-shop";
+import { TwoColumnPage } from '@/components/admin-ui-experimental/layout';
+import { CustomerDetailGeneral } from './customer-detail-general';
+import { CustomerDetailShop } from './customer-detail-shop';
 
-export default async function CustomerDetailPage({ params }: { params: Promise<{ id: string }> }) {
+export default async function CustomerDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   return (
     <div className="flex w-full max-w-[1600px] flex-col gap-y-2 p-3">
@@ -10,6 +14,10 @@ export default async function CustomerDetailPage({ params }: { params: Promise<{
         <CustomerDetailGeneral customerId={id} />
         <CustomerDetailShop userId={id} />
       </TwoColumnPage>
+      <TwoColumnPage>
+        <div></div>
+        <div></div>
+      </TwoColumnPage>
     </div>
-  )
+  );
 }
