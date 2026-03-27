a9925ea884f535e3be5299c231dacf17cffa3db2
jungsikjeong <wndtlr1024@gmail.com>
2026-03-27 11:49:17 +0900

    [Admin-web] medusa-customer이랑 헷갈리지않게 주석으로 안내사항표시

--- apps/admin-web/src/app/(admin)/account/customer/page.tsx
+++ apps/admin-web/src/app/(admin)/account/customer/page.tsx
@@ -1,11 +1,13 @@
 import RouteGuard from '@/components/layout/route-guard';
 import CustomerListTemplate from '@/features/customers/template';
 
+/**
+ * user-service 고객 목록 페이지입니다.
+ * medusa-customer이랑 헷갈리시지 않도록 주의해주세요.
+ */
 export default function CustomerPage() {
   return (
-    <RouteGuard
-      requireRole={['admin', 'master']}
-    >
+    <RouteGuard requireRole={['admin', 'master']}>
       <div className="flex w-full max-w-[1600px] flex-col gap-y-2 p-3">
         <CustomerListTemplate />
       </div>
