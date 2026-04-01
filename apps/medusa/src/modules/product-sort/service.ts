45b9e26b812035a9ff13d863f21601f2281a415f
jungsikjeong <wndtlr1024@gmail.com>
2026-04-01 15:35:09 +0900

    [medusa] 상품 정렬 모듈 및 관련 파일 삭제, 멤버십 기반 상품 필터링을 위한 membership-products API 복원

--- apps/medusa/src/modules/product-sort/service.ts
+++
@@ -1,8 +0,0 @@
-import { MedusaService } from '@medusajs/framework/utils';
-import ProductSortKey from './models/product-sort-key';
-
-class ProductSortModuleService extends MedusaService({
-  ProductSortKey,
-}) {}
-
-export default ProductSortModuleService;
