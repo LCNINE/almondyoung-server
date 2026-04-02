0913f1ad7eafe0dbc774ec059d8347558d2aa750
jungsikjeong <wndtlr1024@gmail.com>
2026-04-02 12:27:16 +0900

    [medusa] 가격정렬 라우트 기능 및 모듈 삭제

--- apps/medusa/src/modules/product-sort/index.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@medusajs/framework/utils';
-import { ProductSortModuleService } from './service';
-
-export const PRODUCT_SORT_MODULE = 'productSort';
-
-export default Module(PRODUCT_SORT_MODULE, {
-  service: ProductSortModuleService,
-});
--- apps/medusa/src/modules/product-sort/service.ts
+++
@@ -1,4 +0,0 @@
-import { MedusaService } from '@medusajs/framework/utils';
-import { ProductSortKey } from './models/product-sort-key';
-
-export class ProductSortModuleService extends MedusaService({ ProductSortKey }) {}
