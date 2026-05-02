52e6479f8fc6fc56f7c6ef3bcc8c785af35fa943
PauseB <116489192+PauseB@users.noreply.github.com>
2026-05-02 17:46:16 +0900

    [user-service[ 표준 OIDC 클라이언트 요구사항 구현

---
+++ apps/user-service/src/api/well-known/well-known.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WellKnownController } from './well-known.controller';
+import { WellKnownService } from './well-known.service';
+
+@Module({
+  imports: [ConfigModule],
+  controllers: [WellKnownController],
+  providers: [WellKnownService],
+})
+export class WellKnownModule {}
