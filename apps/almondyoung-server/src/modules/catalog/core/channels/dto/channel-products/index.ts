2550cd99be1aeb0f1b8594205a3ac8211e77f98e
PauseB <116489192+PauseB@users.noreply.github.com>
2026-05-13 06:43:21 +0900

    Squashed commit of the following:
    commit 054e4e04aa70fffc1876e9994ddfc1e106e52128
    Author: PauseB <116489192+PauseB@users.noreply.github.com>
    Date:   Wed May 13 06:38:05 2026 +0900
    
        df 환경 제거
    
    commit 513edea8fa689054add510a3160d3506a8ef6144
    Author: PauseB <116489192+PauseB@users.noreply.github.com>
    Date:   Wed May 13 00:55:32 2026 +0900
    
        almondyoung-server 앱을 core로 이름 변경, 레거시 앱 제거

--- apps/almondyoung-server/src/modules/catalog/core/channels/dto/channel-products/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './create-channel-product.dto';
-export * from './update-channel-product.dto';
-export * from './channel-product-response.dto';
-export { SalesChannelDto } from '../sales-channels/sales-channel-response.dto';
--- apps/almondyoung-server/src/modules/catalog/core/channels/dto/sales-channels/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './create-sales-channel.dto';
-export * from './update-sales-channel.dto';
-export * from './sales-channel-response.dto';
