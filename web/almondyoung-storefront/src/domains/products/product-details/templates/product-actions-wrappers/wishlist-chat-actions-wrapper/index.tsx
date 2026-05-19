33396494c02d68ce719614cc066f182c56e479c7
jungsikjeong <wndtlr1024@gmail.com>
2026-05-19 18:02:38 +0900

    [storefront] 로그인중일때만 위시리스트 눌렀는지 여부 불러오기

--- web/almondyoung-storefront/src/domains/products/product-details/templates/product-actions-wrappers/wishlist-chat-actions-wrapper/index.tsx
+++ web/almondyoung-storefront/src/domains/products/product-details/templates/product-actions-wrappers/wishlist-chat-actions-wrapper/index.tsx
@@ -1,17 +1,22 @@
 import { getWishlistByProductId } from "@/lib/api/users/wishlist"
+import { Customer } from "@/lib/types/ui/medusa"
 import { WishlistButton } from "../../../components/actions/wishlist-button"
 // import { ChatButton } from "./chat-button"
 
 interface Props {
   productId: string
   countryCode: string
+  customer: Customer | null
 }
 
 export async function WishlistChatActionsWrapper({
   productId,
   countryCode,
+  customer,
 }: Props) {
-  const wishlist = await getWishlistByProductId(productId).catch(null)
+  const wishlist = customer
+    ? await getWishlistByProductId(productId).catch(() => null)
+    : null
 
   return (
     <div className="flex items-center gap-2">
