1e882e9a2ed6e1f6190a808329b806f0886e536b
Pear-HyunJI <ponyo3764@gmail.com>
2026-05-20 18:08:13 +0900

    [Medusa] customer-promotion 링크 many-to-many로 수정
    isList: true를 양쪽에 설정해 한 쿠폰을 여러 고객에게 발급 가능하도록 변경.
    기존 정의(한쪽만 isList: true)는 promotion 1개가 customer 1명에게만 할당 가능해서
    두 번째 발급 시 400 에러 발생.

--- apps/medusa/src/links/customer-promotion.ts
+++ apps/medusa/src/links/customer-promotion.ts
@@ -2,7 +2,9 @@ import { defineLink } from '@medusajs/framework/utils';
 import CustomerModule from '@medusajs/medusa/customer';
 import PromotionModule from '@medusajs/medusa/promotion';
 
-export default defineLink(CustomerModule.linkable.customer, {
-  linkable: PromotionModule.linkable.promotion,
-  isList: true,
-});
+// isList: true on both sides = many-to-many
+// 한 쿠폰을 여러 고객에게 발급하고, 한 고객이 여러 쿠폰을 가질 수 있음
+export default defineLink(
+  { linkable: CustomerModule.linkable.customer, isList: true },
+  { linkable: PromotionModule.linkable.promotion, isList: true },
+);
