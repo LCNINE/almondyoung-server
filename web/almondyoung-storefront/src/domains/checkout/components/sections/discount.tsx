2296d9769531904a4c5f8762daddeba4443e2c3f
jungsikjeong <wndtlr1024@gmail.com>
2026-05-26 15:13:27 +0900

    [Storefront] 로그제거

--- web/almondyoung-storefront/src/domains/checkout/components/sections/discount.tsx
+++ web/almondyoung-storefront/src/domains/checkout/components/sections/discount.tsx
@@ -49,8 +49,6 @@ export const DiscountSection = ({
     appliedPromotionCode ?? ""
   )
 
-  console.log("promotions:", promotions)
-
   const handleCouponChange = useCallback(
     (code: string) => {
       startTransition(async () => {
