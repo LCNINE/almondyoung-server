aa8e568b0260fb6784ee4697405488667f10eb2b
Pear-HyunJI <ponyo3764@gmail.com>
2026-06-10 13:24:29 +0900

    빌드에러

--- apps/admin-web/src/app/(admin)/inventory/holders/page.tsx
+++ apps/admin-web/src/app/(admin)/inventory/holders/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import RouteGuard from '@/components/layout/route-guard';
 import HoldersTemplate from '@/features/inventory/holders/template';
 
@@ -5,7 +6,9 @@ export default function Page() {
   return (
     <RouteGuard requireRole={['admin', 'master']}>
       <div className="flex w-full max-w-[1600px] flex-col gap-y-2 p-3">
-        <HoldersTemplate />
+        <Suspense>
+          <HoldersTemplate />
+        </Suspense>
       </div>
     </RouteGuard>
   );
