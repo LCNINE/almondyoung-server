cf836905313f45d074e1423fcf200496d2717580
jungsikjeong <wndtlr1024@gmail.com>
2026-06-11 15:00:51 +0900

    [Admin-web] : 제품 업데이트 및 삭제 기능 개선, 상태 변경 로직 추가

--- apps/core/src/modules/catalog/operations/bulk/bulk.module.ts
+++ apps/core/src/modules/catalog/operations/bulk/bulk.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductBulkController } from './product-bulk.controller';
 import { ProductBulkService } from './product-bulk.service';
+import { ProductsModule } from '../../core/products/products.module';
 
 @Module({
+  imports: [ProductsModule],
   controllers: [ProductBulkController],
   providers: [ProductBulkService],
   exports: [ProductBulkService],
