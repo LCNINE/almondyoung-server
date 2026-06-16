7622839e6a45fd4ef876336f10a5429b646a912f
PauseB <116489192+PauseB@users.noreply.github.com>
2026-06-16 21:29:49 +0900

    [medusa] pool size 증량

--- apps/medusa/medusa-config.js
+++ apps/medusa/medusa-config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
       },
       pool: {
         min: 2,
-        max: 10,
+        max: 20,
         idleTimeoutMillis: 30000,
         acquireTimeoutMillis: 10000, // 연결 획득 타임아웃
       },
