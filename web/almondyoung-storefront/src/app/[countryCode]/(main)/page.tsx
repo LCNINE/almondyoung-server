7c0e976083387d505da320075805fa5f6b7a50de
Pear-HyunJI <ponyo3764@gmail.com>
2026-06-18 18:58:54 +0900

    일단 카페24 연동 배너 팝업 주석

--- web/almondyoung-storefront/src/app/[countryCode]/(main)/page.tsx
+++ web/almondyoung-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,7 +1,7 @@
 import { ThemeManager } from "@/components/shared/theme-manager"
 import { SurveyPromptBanner } from "@/components/survey-prompt-banner"
-import { Cafe24LinkBanner } from "@/components/cafe24-link-banner"
-import { Cafe24LinkPopup } from "@/components/layout/cafe24-link-popup"
+// import { Cafe24LinkBanner } from "@/components/cafe24-link-banner"
+// import { Cafe24LinkPopup } from "@/components/layout/cafe24-link-popup"
 import { getMyProfile } from "@/lib/api/users/profile"
 import { siteConfig } from "@/lib/config/site"
 import { getSEOTags } from "@/lib/seo"
@@ -34,8 +34,8 @@ export default async function Home({
       {showSurvey && <SurveyPromptBanner countryCode={countryCode} />}
 
       {/* 카페24 계정 미연동 유저 대상 연동 권장 팝업 + 배너 (연동 여부는 클라이언트에서 확인) */}
-      {isLoggedIn && <Cafe24LinkPopup countryCode={countryCode} />}
-      {isLoggedIn && <Cafe24LinkBanner countryCode={countryCode} />}
+      {/* {isLoggedIn && <Cafe24LinkPopup countryCode={countryCode} />} */}
+      {/* {isLoggedIn && <Cafe24LinkBanner countryCode={countryCode} />} */}
 
       {/* 테마 매니저 (개발 모드에서만 표시) */}
       {process.env.NODE_ENV === "development" && <ThemeManager />}
