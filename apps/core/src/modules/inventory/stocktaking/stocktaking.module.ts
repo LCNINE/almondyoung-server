236bff2d0f0b3e3133c6ffb8766fdbd74c356af6
PauseB <116489192+PauseB@users.noreply.github.com>
2026-07-09 02:55:55 +0900

    [core] 실사 완료 원장 원자 적용 + 미리보기 격하 + 직접INSERT 봉인 (P0-2/P0-3/W3)

--- apps/core/src/modules/inventory/stocktaking/stocktaking.module.ts
+++ apps/core/src/modules/inventory/stocktaking/stocktaking.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StocktakingController } from './controllers/stocktaking.controller';
 import { StocktakingService } from './services/stocktaking.service';
+import { CoreInventoryModule } from '../core/inventory.module';
 
 @Module({
+  imports: [CoreInventoryModule],
   controllers: [StocktakingController],
   providers: [StocktakingService],
   exports: [StocktakingService],
